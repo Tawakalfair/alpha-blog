568e54aa17104425d5beeabd53c3823642c78be5
Iqbal Tawakal <fairtawakal@gmail.com>
2018-03-30 13:43:06 +0700

    menampilkan data insert database

--- app/controllers/Artikels_controller.rb
+++ app/controllers/Artikels_controller.rb
@@ -7,14 +7,21 @@ end
 def create
 #render plain: params[:artikel].inspect
 @artikel = Artikel.new(artikel_params)
-@artikel.save
-redirect_to artikels_show(@artikel)
+if @artikel.save
+flash[:notice]="artikel berhasil disimpan"
+redirect_to artikel_path(@artikel)
+
+else
+render 'new'
+end
+end
+
+def show
+@artikel= Artikel.find(params[:id])
 end
 
 private
 def artikel_params
 params.require(:artikel).permit(:judul,:deskripsi)
 end
-
-
 end
